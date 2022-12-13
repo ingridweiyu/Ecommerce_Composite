4479bab131794029a01901c2f0e7230d18ca62cb
Qiao Huang <qh2234@columbia.edu>
2022-12-13 15:27:45 -0500

    Connect to User micro-service

--- user.py
+++ user.py
@@ -1,35 +1,45 @@
 from flask_login import UserMixin
 
-from db import get_db
+import requests, json
+
+USER_URL = "ec2-3-87-226-6.compute-1.amazonaws.com/"
 
 
 class User(UserMixin):
-    def __init__(self, id_, name, email, profile_pic):
+    def __init__(self, id_, first_name, last_name, email, profile_pic):
         self.id = id_
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.name = f"{self.first_name} {self.last_name}"
         self.email = email
         self.profile_pic = profile_pic
 
     @staticmethod
-    def get(user_id):
-        db = get_db()
-        user = db.execute(
-            "SELECT * FROM user WHERE id = ?", (user_id,)
-        ).fetchone()
-        if not user:
+    def parseJSON(json_str):
+        obj = json.loads(json_str)
+        users = obj.data
+        if len(users) <= 0:
             return None
-
-        user = User(
-            id_=user[0], name=user[1], email=user[2], profile_pic=user[3]
-        )
-        return user
+        else:
+            user = users[0].data
+            return User(
+                user.user_id, user.first_name, user.last_name, None, user.picture
+            )
 
     @staticmethod
-    def create(id_, name, email, profile_pic):
-        db = get_db()
-        db.execute(
-            "INSERT INTO user (id, name, email, profile_pic)"
-            " VALUES (?, ?, ?, ?)",
-            (id_, name, email, profile_pic),
-        )
-        db.commit()
+    def get(user_id):
+        response = requests.get(USER_URL + f"?google_id={user_id}")
+        if response.status_code == 404:
+            return None
+        else:
+            return User.parseJSON(response.json())
+
+    @staticmethod
+    def create(id_, first_name, last_name, email, profile_pic):
+        obj = {
+            "google_id": id_,
+            "first_name": first_name,
+            "last_name": last_name,
+            "picture": profile_pic,
+        }
+        requests.post(USER_URL, data=json.dumps(obj))
